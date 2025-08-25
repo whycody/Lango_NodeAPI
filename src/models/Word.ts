17063ae615403acd51829e978cc26e2a4f2c7ae0
Oktawian Kausz <play26400@gmail.com>
2025-08-25 18:01:31 +0200

    Remove not used props from Word schema

--- src/models/Word.ts
+++ src/models/Word.ts
@@ -8,12 +8,7 @@ interface Word extends Document {
   firstLang: string;
   secondLang: string;
   source: string;
-  interval: number;
   addDate: Date;
-  repetitionCount: number;
-  lastReviewDate: Date;
-  nextReviewDate: Date;
-  EF: number;
   updatedAt: Date;
   active: boolean;
   removed: boolean;
@@ -27,12 +22,7 @@ const wordSchema = new Schema<Word>({
   firstLang: { type: String, required: true },
   secondLang: { type: String, required: true },
   source: { type: String, required: true },
-  interval: { type: Number, default: 1 },
   addDate: { type: Date, default: Date.now },
-  repetitionCount: { type: Number, default: 0 },
-  lastReviewDate: { type: Date, default: Date.now },
-  nextReviewDate: { type: Date, default: Date.now },
-  EF: { type: Number, default: 2.5 },
   updatedAt: { type: Date, default: Date.now },
   active: { type: Boolean, default: true },
   removed: { type: Boolean, default: false },
