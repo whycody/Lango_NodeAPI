320baf64bb0ca868e9ab6bac696a26499975899b
Oktawian Kausz <play26400@gmail.com>
2025-11-25 01:38:35 +0100

    Remove temporary information about mongo_uri when connecting to DB

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ const connectWithRetry = async (delay = 5000) => {
       console.log('Connected to MongoDB');
       break;
     } catch (err) {
-      console.error('MongoDB connection failed, retrying in 5s...', process.env.MONGO_URI);
+      console.error('MongoDB connection failed, retrying in 5s...');
       await new Promise(res => setTimeout(res, delay));
     }
   }
