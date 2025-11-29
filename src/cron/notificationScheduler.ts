711a1eda39c26d4920f9279d329c8297dbf854ad
Oktawian Kausz <play26400@gmail.com>
2025-11-29 18:00:04 +0100

    Remove logs, fix cron

--- src/cron/notificationScheduler.ts
+++ src/cron/notificationScheduler.ts
@@ -3,7 +3,7 @@ import User from '../models/core/User';
 import { Types } from "mongoose";
 import { calculateBestTimes } from "../services/notifications/calculateBestTime";
 
-cron.schedule('* * * * *', async () => {
+cron.schedule('0 3 * * *', async () => {
   try {
     console.log('Updating notification times for users...');
 
